218719a6a3db8f5a0dc918e01f1da4c8215c7657
sohagbanik <baniksohag2006@gmail.com>
2026-07-15 00:02:06 +0530

    Configure webpack to ignore dynamic require warnings from stellar-sdk transitive dependencies

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,6 +7,21 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: false,
   },
+  webpack: (config) => {
+    // Suppress warning "Critical dependency: the request of a dependency is an expression"
+    config.module = config.module || {};
+    config.module.exprContextCritical = false;
+
+    // Ignore specific warnings regarding dynamic requires in node modules
+    config.ignoreWarnings = [
+      ...(config.ignoreWarnings || []),
+      { module: /node_modules\/require-addon/ },
+      { module: /node_modules\/sodium-native/ },
+      { message: /Critical dependency/ },
+    ];
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
